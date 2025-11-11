dfa26c0b05a48664bdd34d36a4371b41138b72ae
ammardyaakob <ammardanielyaakob@gmail.com>
2025-11-11 01:48:03 +0000

    feat/fix: add consumerChild and fix types of parameters from int to queue_t

--- producers.c
+++ producers.c
@@ -3,8 +3,9 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
-#define Q_SIZE 20
-#define NUM_PRODUCERS 20
+#define Q_SIZE 10
+#define NUM_PRODUCERS 5
+#define NUM_CONSUMERS 5
 
 typedef struct {
     int message;
@@ -16,15 +17,29 @@ typedef struct {
     int head, tail, count;
 } queue_t;
 
-void producerChild(int id, int* shared_queue){
-    printf("Im child %d \n", id);
+void producerChild(int id, queue_t* shared_queue){
+    printf("Im producer child %d looking at address queue %d \n", id, shared_queue);
 }
 
-void instantiateProducers(int num_producers, queue_t shared_queue){
+void consumerChild(int id, queue_t* shared_queue){
+    printf("Im consumer child %d looking at address queue %d \n", id, shared_queue);
+}
+
+void instantiateProducers(int num_producers, queue_t *shared_queue){
     for (int i = 0; i < num_producers; i++) {
         pid_t pid = fork();
         if (pid == 0) {
-            producerChild(i, &shared_queue);
+            producerChild(i, shared_queue);
+            exit(0);
+        }
+    }
+}
+
+void instantiateConsumers(int num_producers, queue_t *shared_queue){
+    for (int i = 0; i < num_producers; i++) {
+        pid_t pid = fork();
+        if (pid == 0) {
+            consumerChild(i, shared_queue);
             exit(0);
         }
     }
@@ -33,6 +48,8 @@ void instantiateProducers(int num_producers, queue_t shared_queue){
 int main(int argc, char *argv[]) {
     printf("Hello world!\n");
     queue_t shared_queue;
-    int num_producers = 10;
-    instantiateProducers(num_producers, shared_queue);
+    int num_producers = NUM_PRODUCERS;
+    int num_consumers = NUM_CONSUMERS;
+    instantiateProducers(num_producers, &shared_queue);
+    instantiateConsumers(num_consumers, &shared_queue);
 };
