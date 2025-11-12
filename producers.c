14b66d4716f476fa2736867882b1686b594f7595
ammardyaakob <ammardanielyaakob@gmail.com>
2025-11-12 13:47:02 +0000

    feat: add development to child processes - add more default values - rename entries as message_t tuple - add seed to child processes using child pid
    - make parent process wait for all children to terminate

--- producers.c
+++ producers.c
@@ -2,27 +2,39 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/wait.h>
 
 #define Q_SIZE 10
-#define NUM_PRODUCERS 5
-#define NUM_CONSUMERS 5
+#define NUM_PRODUCERS 10
+#define NUM_CONSUMERS 3
+#define MAX_ENTRIES 20
+#define MAX_PROD_MESSAGES 9
+#define MAX_PROD_WAIT 2
+#define MAX_CONS_WAIT 4
 
 typedef struct {
-    int message;
+    int data_value;
     int priority;
-} entry_t;
+} message_t;
 
 typedef struct {
-    entry_t entries[Q_SIZE];
+    message_t entries[Q_SIZE];
     int head, tail, count;
 } queue_t;
 
 void producerChild(int id, queue_t* shared_queue){
-    printf("Im producer child %d looking at address queue %d \n", id, shared_queue);
+    srand(getpid());
+    int priority = rand() % 10;
 }
 
 void consumerChild(int id, queue_t* shared_queue){
-    printf("Im consumer child %d looking at address queue %d \n", id, shared_queue);
+    srand(getpid());
+    for (int i = 0; i < 4; i++){
+        printf("Consumer %d read\n", id);
+        sleep(rand() % MAX_CONS_WAIT + 1);
+    }
+
 }
 
 void instantiateProducers(int num_producers, queue_t *shared_queue){
@@ -46,10 +58,19 @@ void instantiateConsumers(int num_producers, queue_t *shared_queue){
 }
 
 int main(int argc, char *argv[]) {
-    printf("Hello world!\n");
+
+    int status;
+    srand(time(NULL));
     queue_t shared_queue;
     int num_producers = NUM_PRODUCERS;
     int num_consumers = NUM_CONSUMERS;
+    
     instantiateProducers(num_producers, &shared_queue);
     instantiateConsumers(num_consumers, &shared_queue);
+
+    // Wait for all children to terminate.
+    for (int i = 0; i < NUM_CONSUMERS + NUM_PRODUCERS; i++){
+        waitpid(-1, &status, 0);
+    }
+    
 };
